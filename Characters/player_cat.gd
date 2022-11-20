082f07f3ef8eb090bca25a05ad709f5567601625
maemreyo <ngonhuthanhtrung1409@gmail.com>
2022-11-20 07:26:30 +0700

    fix: Update new animation state when walking

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var starting_direction : Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
-
+@onready var state_machine = animation_tree.get("parameters/playback")
 func _ready():
 #	animation_tree.set("parameters/Idle/blend_position", starting_direction)
 	update_animation_parameters(starting_direction)
@@ -22,9 +22,18 @@ func _physics_process(_delta):
 	
 	# Move and slide function uses velocity of character body to move character on map
 	move_and_slide()
+	
+	# Pick a new state for the player's animation
+	pick_new_state()
 
 func update_animation_parameters(move_input: Vector2):
 	# Dont change animation parameters if there is no move input
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
